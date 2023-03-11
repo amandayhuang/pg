3284ca6a075ba408a9b22785758277d537a437d4
a-yh <103937002+a-yh@users.noreply.github.com>
2023-03-11 18:22:11 -0500

    make game work

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,55 @@
 import "./App.css";
-import { Box } from "@material-ui/core";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { Box, Button, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { getRandomWordAndLetters } from "./utils/words";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  console.log("WORD", getRandomWordAndLetters());
+  const [guess, setGuess] = useState("");
+  const [word, setWord] = useState("");
+  const [letters, setLetters] = useState([]);
+  const [result, setResult] = useState("");
+  const [rounds, setRounds] = useState(1);
+
+  const letterHandler = (letter) => {
+    setGuess(guess + letter);
+  };
+
+  const deleteHandler = () => {
+    setGuess(guess.substring(0, guess.length - 1));
+  };
+
+  const submitHandler = () => {
+    if (guess === word) {
+      setResult(`Congrats, the correct word is ${word}!`);
+      setRounds(rounds + 1);
+    } else {
+      setResult(`Try again ${guess} is not the correct word.`);
+    }
+    setGuess("");
+  };
+
+  useEffect(() => {
+    const { word, letters } = getRandomWordAndLetters();
+    setWord(word);
+    setLetters(letters);
+    console.log("WORD", word);
+  }, [rounds]);
+
   return (
-    <Box display="flex" justifyContent="center" flexDirection="column">
-      <p className="title">mini games to play by yourself or with others</p>
-      <Link to="/height">
-        <p variant="outlined" className="title game">
-          {" "}
-          guess the famous person's height{" "}
-          <ArrowForwardIosIcon className="arrow" />
-        </p>
-      </Link>
-      <Link to="/keyword">
-        <p variant="outlined" className="title game">
-          {" "}
-          text message keyword search <ArrowForwardIosIcon className="arrow" />
-        </p>
-      </Link>
-      <Link to="/art">
-        <p variant="outlined" className="title game">
-          {" "}
-          learn about art <ArrowForwardIosIcon className="arrow" />
-        </p>
-      </Link>
-    </Box>
+    <>
+      <Typography>{guess}</Typography>
+      <Typography>{result}</Typography>
+      {letters.map((letter) => (
+        <Button
+          onClick={guess.length < 8 ? () => letterHandler(letter) : undefined}
+        >
+          {letter}
+        </Button>
+      ))}
+      {guess.length > 0 && <Button onClick={deleteHandler}>Delete</Button>}
+      {guess.length === 8 && <Button onClick={submitHandler}>Submit</Button>}
+    </>
   );
 };
 
